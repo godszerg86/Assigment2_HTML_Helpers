fc8943e511c42e3b543e5b125117f965956f9084
Sergei <sbelialov@gmail.com>
2018-10-26 10:36:44 -0500

    DONE: DateTIme.ToAge()

---
+++ Assigment2_HTML_Helpers/Classes/DateTimeExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assigment2_HTML_Helpers.Classes
+{
+    public static class DateTimeExtension 
+    {
+        public static int ToAge(this DateTime date)
+        {
+
+            TimeSpan diff = DateTime.Now - date;
+            return diff.Days/365;
+        }
+
+    }
+}
--- Assigment2_HTML_Helpers/Controllers/HomeController.cs
+++ Assigment2_HTML_Helpers/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Assigment2_HTML_Helpers.Classes;
 
 namespace Assigment2_HTML_Helpers.Controllers
 {
@@ -10,6 +11,9 @@ namespace Assigment2_HTML_Helpers.Controllers
     {
         public ActionResult Index()
         {
+            var age1 = new DateTime(1990, 01, 01).ToAge();
+            var age2 = new DateTime(1987, 05, 07).ToAge();
+            var age3 = new DateTime(1975, 10, 10).ToAge();
             return View();
         }
 
