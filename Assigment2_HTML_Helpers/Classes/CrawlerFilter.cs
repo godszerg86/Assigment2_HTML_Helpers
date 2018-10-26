d8779a8ad2a3e6f640e419495097955c391599b0
Sergei <sbelialov@gmail.com>
2018-10-26 10:18:22 -0500

    DONE: Action filter for crawlers

---
+++ Assigment2_HTML_Helpers/Classes/CrawlerFilter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assigment2_HTML_Helpers.Classes
+{
+    public class CrawlerFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.Request.Browser.Crawler)
+            {
+                filterContext.Result = new ViewResult() { ViewName = "Crawler" };
+            }
+        }
+    }
+}
